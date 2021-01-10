aa331bdc9a2c908c9084802d1f590eaa5795ae23
villalbaweb <villalbaweb@gmail.com>
2021-01-10 13:23:17 -0600

    Adding trigger coillision systems

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) 
+    {
+        print($"{gameObject.name} trigger into {other.gameObject.name}");
+    }
+}
