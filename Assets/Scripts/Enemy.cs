dd6b67dfdcadc218ce13d14fcefac50452492ef2
villalbaweb <villalbaweb@gmail.com>
2021-01-17 08:43:34 -0600

    Adding score board concept

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,12 +4,22 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX = null;
     [SerializeField] Transform runtimeSpawnedParent = null;
+    [SerializeField] int scorePoints = 5;
+
+    ScoreBoard _scoreBoard;
+
+    private void Awake() 
+    {
+        _scoreBoard = FindObjectOfType<ScoreBoard>();    
+    }
 
     private void OnParticleCollision(GameObject other) 
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = runtimeSpawnedParent;
-        
+
+        _scoreBoard?.IncreaseScore(scorePoints);
+
         Destroy(gameObject);    
     }
 }
---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+
+    public void IncreaseScore(int amountToIncrease)
+    {
+        score += amountToIncrease;
+        print($"Score increase to {score}");
+    }
+}
