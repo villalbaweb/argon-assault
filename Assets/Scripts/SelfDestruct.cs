1a382baa4f970827fe14bc309262d4635b52d393
villalbaweb <villalbaweb@gmail.com>
2021-01-17 08:33:48 -0600

    Adding self destruct script to enemy explosion particle system

---
+++ Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    [SerializeField] float timeToDestroy = 3f;
+
+    private void Awake() 
+    {
+        Destroy(gameObject, timeToDestroy);
+    }
+}
