ffd0eb5324bd84128188841ffa5ae667c8885d92
villalbaweb <villalbaweb@gmail.com>
2021-01-17 08:26:27 -0600

    Adding enemy destroy paticle system

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,14 @@
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX = null;
+    [SerializeField] Transform runtimeSpawnedParent = null;
+
     private void OnParticleCollision(GameObject other) 
     {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = runtimeSpawnedParent;
+        
         Destroy(gameObject);    
     }
 }
